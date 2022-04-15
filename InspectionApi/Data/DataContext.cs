bb61157c221cb0470a3a691849132257c1b53730
krishnamohanparuchuri <paruchurikris@gmail.com>
2022-04-15 22:52:45 +0200

    Add project files.

---
+++ InspectionApi/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using InspectionApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InspectionApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+       public  DbSet<Inspection>  Inspections { get; set; }
+       public DbSet<InspectionType> InspectionTypes { get; set; }
+       public DbSet<Status> Statuses { get; set; }
+    }
+}
---
+++ InspectionApi/Models/Inspection.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectionApi.Models
+{
+    public class Inspection
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Status { get; set; } = String.Empty;
+
+        [StringLength(200)]
+        public string Comments { get; set; } = String.Empty;
+
+
+        public int InspectionId { get; set; }
+        public InspectionType? InspectionType { get; set; }
+    }
+}
